550491dae0c6fa189a577e66eff4eb3605009578
Plaga-Devs <ContactPlaga2018@gmail.com>
2018-04-14 17:42:05 -0500

    Part 4 Complete
    Made migrations and debugged.
    
    Running locally now.

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -19,9 +19,11 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$',views.HomePage.as_view(),name='home'),
-    url(r'^accounts/',include('accounts.urls',namespace='accounts')), # connects to urls.py in accounts
-    url(r'^accounts/',include('django.contrib.auth.urls')), # connects to django authorization stuff builtins
+    url(r"^$", views.HomePage.as_view(), name="home"),
+    url(r"^test/$", views.TestPage.as_view(), name="test"),
+    url(r"^thanks/$", views.ThanksPage.as_view(), name="thanks"),
+    url(r"^admin/", admin.site.urls),
+    url(r"^accounts/", include("accounts.urls", namespace="accounts")),
+    url(r"^accounts/", include("django.contrib.auth.urls")),
 
 ]
--- simplesocial/simplesocial/views.py
+++ simplesocial/simplesocial/views.py
@@ -1,6 +1,13 @@
 # basically create a real simple view for the home page
 from django.views.generic import TemplateView
 
+
+class TestPage(TemplateView):
+    template_name = 'test.html'
+
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
 class HomePage(TemplateView):
     template_name = 'index.html'
     # Linking to urls.py
